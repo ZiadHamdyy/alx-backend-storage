7696bb6c2aa039534838dcaaa867948060c31dc1
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-04-24 18:13:27 +0200

    task 1, 2

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,4 @@
+-- Write a SQL script that creates a table users following these requirements:
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
